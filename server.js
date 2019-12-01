be1d55901795362c998bf8e98cc12089ff0c13aa
Glitch (arrow1802-boilerplate-project-timestamp) <none>
2019-12-01 11:24:07 +0000

    🛎🐡 Checkpoint ./server.js:738909/107

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 app.get("/api/timestamp/:date_string?", function (req, res) {
-  console.log(req)
-  res.json({result:req.date_string});
+  console.log(req.params)
+  res.json(req.params);
 });
 
 
