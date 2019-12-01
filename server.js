384b313fb4eee664116a11f78a31f78bd47b72a2
Glitch (arrow1802-boilerplate-project-timestamp) <none>
2019-12-01 12:22:11 +0000

    💿🙈 Checkpoint ./server.js:738909/516

--- server.js
+++ server.js
@@ -25,12 +25,12 @@ app.get("/api/hello", function (req, res) {
 });
 app.get("/api/timestamp/:date_string?", function (req, res) {
    
-  let value= req.params.date_string != undefined ? req.params.date_string : new Date().format()
+  let value= req.params.date_string != undefined ? req.params.date_string : new Date().toISOString().slice(0,10)
   let date = new Date(value)    //{"unix":1451001600000,"utc":"Fri, 25 Dec 2015 00:00:00 GMT"}
   // date = new Date(date*1)  //{"unix":1450137600000,"utc":"Tue, 15 Dec 2015 00:00:00 GMT"}
-  console.log("params :", req.params.date_string,"  value :",value) 
-  let utc = date.toUTCString()
-  let unix = date.getTime()
+  console.log("params :", req.params.date_string,"  value :",new Date().toISOString().slice(0,10)) 
+  let utc = value.toUTCString()
+  let unix = value.getTime()
   res.json({unix:unix,utc:utc});
 });
 
